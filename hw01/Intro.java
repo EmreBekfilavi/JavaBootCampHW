c07ba0f8ae73d5f7fdfaa098fb9348b776702787
EmreBekfilavi <embek_95@hotmail.com>
2022-09-18 23:00:43 +0300

    Canlı dersin kod uygulaması.

---
+++ hw01/Intro.java
@@ -0,0 +1,41 @@
+package hw01;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		String ortaMetin = "İlginizi çekebilir";
+		String altMetin = "Vade süresi";
+
+		System.out.println(ortaMetin);
+
+		int vade = 12;
+
+		double dolarDun = 18.32;
+		double dolarBugun = 18.30;
+
+		boolean dolarDustumu = false;
+
+		String okYonu = "";
+
+		if (dolarBugun < dolarDun) {
+			okYonu = "down.svg";
+			System.out.println(okYonu);
+		} else if (dolarBugun > dolarDun) {
+			okYonu = "up.svg";
+			System.out.println(okYonu);
+		} else {
+			okYonu = "equal.svg";
+			System.out.println(okYonu);
+		}
+
+		String[] krediler = { "Hızlı Kredi", "Maaşını Halkbanktan", "Mutlu Emekli" };
+
+		for (int i = 0; i < krediler.length; i++) {
+			System.out.println(krediler[i]);
+		}
+
+	}
+
+}
