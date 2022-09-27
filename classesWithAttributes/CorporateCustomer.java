d30d4e1011b86dafb0205ac315a709e61cb5c139
EmreBekfilavi <embek_95@hotmail.com>
2022-09-27 20:34:25 +0300

    classesWithAttributes uygulaması, inheritanceDemo uygulaması ve getter setter uygulamaları

---
+++ classesWithAttributes/CorporateCustomer.java
@@ -0,0 +1,24 @@
+package classesWithAttributes;
+
+public class CorporateCustomer extends Customer {
+
+	private String taxNumber;
+	private String companyName;
+
+	public String getTaxNumber() {
+		return taxNumber;
+	}
+
+	public void setTaxNumber(String taxNumber) {
+		this.taxNumber = taxNumber;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+}
---
+++ classesWithAttributes/Customer.java
@@ -0,0 +1,33 @@
+package classesWithAttributes;
+
+public class Customer {
+
+	private int id;
+	private String customerNumber;
+	private String phone;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCustomerNumber() {
+		return customerNumber;
+	}
+
+	public void setCustomerNumber(String customerNumber) {
+		this.customerNumber = customerNumber;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
